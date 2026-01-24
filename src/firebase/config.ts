f9de5f0c1425eb2e3cb8582f6d03c744cf339f3e
Faizan Mujahid <faizanmujahid100@gmail.com>
2026-01-24 04:35:04 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // This file is populated with your Firebase configuration by the system.
 // DO NOT EDIT
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID",
+  "projectId": "studio-7355461442-45a09",
+  "appId": "1:964570854530:web:01ab6e81716aabad7eb09b",
+  "apiKey": "AIzaSyBe3SvhMZv69fp2MHcHfoydKNMvFBptrKk",
+  "authDomain": "studio-7355461442-45a09.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "964570854530"
 };
