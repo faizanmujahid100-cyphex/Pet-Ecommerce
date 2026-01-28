e3a6d0a24b5de3c8950cbafba5e180a3152ff71f
Faizan Mujahid <faizanmujahid100@gmail.com>
2026-01-28 16:13:16 +0000

    cloudanary preset=productscat

--- src/components/admin/product-form.tsx
+++ src/components/admin/product-form.tsx
@@ -266,7 +266,7 @@ export function ProductForm({ product }: { product: Product | null }) {
                       </div>
                     )}
                     <CldUploadButton
-                      uploadPreset="feline_and_friend_preset"
+                      uploadPreset="productscat"
                       options={{
                         sources: ['local', 'camera', 'url'],
                         multiple: false,
@@ -316,7 +316,7 @@ export function ProductForm({ product }: { product: Product | null }) {
                       ))}
                     </div>
                     <CldUploadButton
-                      uploadPreset="feline_and_friend_preset"
+                      uploadPreset="productscat"
                       options={{
                         sources: ['local', 'camera', 'url'],
                         multiple: true,
