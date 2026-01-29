939c04d811b0d51378d697fa5b300a154c5c9756
Faizan Mujahid <faizanmujahid100@gmail.com>
2026-01-29 05:31:40 +0000

    at the time of build in vercel give error 10:28:18.389 Attempted import

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -1,4 +1,4 @@
-import { createNextHandler } from '@genkit-ai/next';
+import { createNextHandler } from 'genkit/next';
 import '@/ai/flows/ai-product-description';
 
 export const POST = createNextHandler();
